f0316a5708cb5162d54f0dd20310c3ac46f7a76d
Amira Elmansy <amira.elmansy@gulfsip.com>
2014-08-18 22:15:01 +0200

    Adding code for prizes and enemies

--- MGWUMinigameTemplate.spritebuilder/Source/MyMinigame.m
+++ MGWUMinigameTemplate.spritebuilder/Source/MyMinigame.m
@@ -12,12 +12,23 @@
 @implementation MyMinigame
 {
     CCPhysicsNode *_physicsNode;
+    CCLabelTTF *_scoreLabel;
+    NSMutableArray *_rocksArray;
+    NSMutableArray *_statuesArray;
+    
+    BOOL _gameOver;
+    int _score;
 }
 
 -(id)init {
     if ((self = [super init])) {
         // Initialize any arrays, dictionaries, etc in here
-        self.instructions = @"These are the game instructions :D";
+        self.instructions = @"Save as many pharaohs as possible and avoid crashing by big rocks";
+        
+        _gameOver = NO;
+        _score = 0;
+        _rocksArray = [NSMutableArray array];
+        _statuesArray = [NSMutableArray array];
     }
     return self;
 }
@@ -25,40 +36,159 @@
 -(void)didLoadFromCCB {
     // Set up anything connected to Sprite Builder here
     
-    // We're calling a public method of the character that tells it to jump!
-//    [self.hero jump];
-    
     self.userInteractionEnabled = YES;
     
     _physicsNode.collisionDelegate = self;
-    
-    self.hero.physicsBody.allowsRotation = FALSE;
 }
 
 -(void)onEnter {
     [super onEnter];
     // Create anything you'd like to draw here
     
+    [self schedule:@selector(spawnStatue) interval:2.f];       // Spawn a new statue every .35 seconds
+    [self schedule:@selector(spawnRock) interval:5.f];       // Spawn a new rock every 5 seconds
+    [self schedule:@selector(updateScoreLabel) interval:0.1f];  // Update the score label every 0.1 seconds
+}
+
+- (void)cleanup {
+    
+    //  cleanup is called before our minigame gets deallocated (which happens after we call endMinigameWithScore)
+    //  So we need  to tell the Cocos2d scheduler that we don't need our spawnEnemy and updateScoreLabel methods called anymore
+    [self unscheduleAllSelectors];
+}
+
+-(void)updateScoreLabel
+{
+    _scoreLabel.string = [NSString stringWithFormat:@"%d", _score];
+}
+
+-(void)spawnStatue
+{
+    CCSprite *statue;
+    statue = (CCSprite *)[CCBReader load:@"amiraelmansyStatue"];
+    
+    // Spawn statue on the top of the screen, at a random width
+    statue.position = ccp(self.contentSizeInPoints.width * CCRANDOM_0_1(), self.contentSizeInPoints.height);
+    
+    // Statues have a random scale between 50% and 80%
+    // Here clampf makes sure the random value is between 0.5f and 0.8f
+    statue.scale = clampf(CCRANDOM_0_1(), 0.5f, 0.8f);
+    
+    // Add the statue to the physics node
+    [_physicsNode addChild:statue];
+    
+    //  Add the statue to our statues array so we can track them
+    [_statuesArray addObject:statue];
+    
+    //  Make the statue spin with a random angular impulse
+    //    [rock.physicsBody applyAngularImpulse:CCRANDOM_MINUS1_1() * 15000.0f];
+}
+
+-(void)spawnRock
+{
+    CCSprite *rock;
+    rock = (CCSprite *)[CCBReader load:@"amiraelmansyRock"];
+    
+    // Spawn rocks on the top of the screen, at a random width
+    rock.position = ccp(self.contentSizeInPoints.width * CCRANDOM_0_1(), self.contentSizeInPoints.height);
+    
+    // Rocks have a random scale between 50% and 100%
+    // Here clampf makes sure the random value is between 0.5f and 1.0f
+    rock.scale = clampf(CCRANDOM_0_1(), 0.5f, 1.0f);
+    
+    // Add the rock to the physics node
+    [_physicsNode addChild:rock];
+    
+    //  Add the rock to our rocks array so we can track them
+    [_rocksArray addObject:rock];
+    
+    //  Make the rock spin with a random angular impulse
+//    [rock.physicsBody applyAngularImpulse:CCRANDOM_MINUS1_1() * 15000.0f];
+}
+
+-(void)removeStatue:(CCSprite *)statue withParticles:(BOOL)particles
+{
+    if (particles)
+    {
+        
+    }
+    
+    [statue removeFromParent];
+}
+
+-(void)removeRock:(CCSprite *)rock withParticles:(BOOL)particles
+{
+    if (particles)
+    {
+        
+    }
+    
+    [rock removeFromParent];
 }
 
 -(void)update:(CCTime)delta {
     // Called each update cycle
     // n.b. Lag and other factors may cause it to be called more or less frequently on different devices or sessions
     // delta will tell you how much time has passed since the last cycle (in seconds)
+    
+    if (_gameOver)
+    {
+        [self endMinigame];
+    }
 }
 
 -(void)endMinigame {
     // Be sure you call this method when you end your minigame!
     // Of course you won't have a random score, but your score *must* be between 1 and 100 inclusive
-    [self endMinigameWithScore:arc4random()%100 + 1];
+    NSLog(@"Game Over with Score = %d", _score);
+    [self endMinigameWithScore:_score];
 }
 
 #pragma mark - CCPhysicsCollisionDelegate Callback
 
--(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair typeA:(CCNode *)nodeA typeB:(CCNode *)nodeB
+-(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair HeroCollision:(CCNode *)hero StatueCollision:(CCNode *)statue
 {
-    
+    [[_physicsNode space] addPostStepBlock:^{
+                                                [self removeStatue:(CCSprite *)statue withParticles:NO];
+                                            }
+                                            key:statue];
+    if (_score < 100)
+    {
+        _score += 10;
+    }
+    else
+    {
+        _gameOver = YES;
+    }
 }
+
+-(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair HeroCollision:(CCNode *)hero RockCollision:(CCNode *)rock
+{
+    [[_physicsNode space] addPostStepBlock:^{
+                                                [self removeRock:(CCSprite *)rock withParticles:NO];
+                                            }
+                                            key:rock];
+    
+    //  If we hit a rock, it's game over!
+    _gameOver = YES;
+}
+
+-(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair groundCollision:(CCNode *)ground RockCollision:(CCNode *)rock
+{
+    [[_physicsNode space] addPostStepBlock:^{
+                                                [self removeRock:(CCSprite *)rock withParticles:YES];
+                                            }
+                                            key:rock];
+}
+
+-(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair groundCollision:(CCNode *)ground StatueCollision:(CCNode *)statue
+{
+    [[_physicsNode space] addPostStepBlock:^{
+                                                [self removeStatue:(CCSprite *)statue withParticles:YES];
+                                            }
+                                            key:statue];
+}
+
 
 #pragma mark - Touch Methods
 
